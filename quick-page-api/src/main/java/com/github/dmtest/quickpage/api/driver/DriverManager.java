64d85910caaf154c7d5751394acd5737cbd97602
Dmitry Piskunov <dm.test@mail.ru>
2019-05-04 00:10:43 +0300

    Add screenshot maker

--- quick-page-api/src/main/java/com/github/dmtest/quickpage/api/driver/DriverManager.java
+++ quick-page-api/src/main/java/com/github/dmtest/quickpage/api/driver/DriverManager.java
@@ -1,10 +1,18 @@
 package com.github.dmtest.quickpage.api.driver;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
 
 public interface DriverManager {
 
     WebDriver getDriver();
 
     void quitDriver();
+
+    default File takeScreenshotAsFile() {
+        return ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+    }
 }
